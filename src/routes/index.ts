c3ec77c35924ae7047b9195bb8481f12aa6d7556
Herbert <herbertkayky9@gmail.com>
2023-05-24 16:22:17 -0300

    feat: method delete created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,18 @@
 import {Router} from 'express';
 import { CreateMamiferosController } from '../controllers/CreateMamiferosController';
 import { FindMamiferosController } from '../controllers/FindMamiferosController';
+import { DeleteMamiferosController } from '../controllers/DeleteMamiferosController';
+
 
 const router = Router();
 
 const createMamiferos = new CreateMamiferosController();
 const findMamiferos = new FindMamiferosController();
+const deleteMamiferos = new DeleteMamiferosController()
 
 router.post('/mamifero', createMamiferos.handle)
 router.get('/mamifero/:name', findMamiferos.handle)
+router.delete('/mamifero/:id', deleteMamiferos.handle)
+
 
 export {router}
